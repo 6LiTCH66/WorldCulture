30802d35b5e07d3dfb775ba903c0ab09e72c53ac
Opilane TTHK <opilane@tthk.ee>
2020-12-02 11:21:46 +0200

    Add project files.

---
+++ WorldCulture/WorldCulture/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WorldCulture
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            profileImage.Source = ImageSource.FromFile("world.jpg");
+            aboutlist.ItemsSource = GetMenuItems();
+            var homePage = typeof(Views.HomePage);
+            Detail = new NavigationPage((Page)Activator.CreateInstance(homePage));
+            IsPresented = false;
+        }
+
+        public List<MasterMenu> GetMenuItems()
+        {
+            var list = new List<MasterMenu>();
+            list.Add(new MasterMenu() 
+            { 
+                Text = "Native Americans",
+                Detail = "Native American culture test",
+                ImagePath = "usa.jpg",
+                TargetPage = typeof(Views.America)
+            });
+
+
+            return list;
+        }
+
+        private void aboutlist_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedMenuItem = (MasterMenu)e.SelectedItem;
+            Type selectedPage = selectedMenuItem.TargetPage;
+            Detail = new NavigationPage((Page)Activator.CreateInstance(selectedPage));
+            IsPresented = false;
+        }
+    }
+}
---
+++ WorldCulture/WorldCulture/Views/America.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WorldCulture.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class America : ContentPage
+    {
+        Label lbl;
+        RadioButton radi1, radi2, radi3, radi4;
+
+        public America()
+        {
+           
+
+            lbl = new Label
+            {
+                Text = "What's your favorite animal?"
+            };
+            radi1 = new RadioButton
+            {
+                Text = "Cat",
+            };
+            radi2 = new RadioButton
+            {
+                Text = "Dog",
+            };
+            radi3 = new RadioButton
+            {
+                Text = "Elephant",
+            };
+            radi4 = new RadioButton
+            {
+                Text = "Monkey",
+                IsChecked = true,
+            };
+            StackLayout stackLayout = new StackLayout()
+            {
+                Children ={lbl, radi1 ,radi2, radi3, radi4}
+            };
+            Content = stackLayout;
+        }
+    }
+}
